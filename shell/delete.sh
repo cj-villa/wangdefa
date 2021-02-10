2cafb4f517ddc0de263aa408a020bebb91c779b6
luchenjie <luchenjiemail@gmail.com>
2021-02-10 15:24:53 +0800

    feat: add eslint check

---
+++ shell/delete.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf ./node_modules
+rm -rf ./packages/app/node_modules
+rm -rf ./packages/react-page/node_modules
+rm ./yarn.lock
+rm ./package-lock.json
+rm ./packages/app/package-lock.json
+rm ./packages/react-page/package-lock.json
