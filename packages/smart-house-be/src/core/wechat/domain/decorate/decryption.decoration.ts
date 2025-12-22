a4a34fcef8415fd818db819f629f4f2fa18e233e
luchenjie <luchenjie@gumingnc.com>
2025-12-22 14:18:58 +0800

    feat: add message

--- packages/smart-house-be/src/core/wechat/domain/decorate/decryption.decoration.ts
+++ packages/smart-house-be/src/core/wechat/domain/decorate/decryption.decoration.ts
@@ -18,7 +18,7 @@ const logger = new ConsoleLogger('DecryptionDecorate');
 const getDecryptionVO = (request: Request<{}, {}, any, SubscriptionDecryptionRequestQueryDto>) => {
   const token = KvService.get('token', ['subscription', 'token']);
   const encodingAESKey = KvService.get('token', ['subscription', 'encodingAESKey']);
-  console.log(`body: ${JSON.stringify(request.body.xml)} query: ${request.query}`);
+  console.log(`body: ${JSON.stringify(request.body.xml)} query: ${JSON.stringify(request.query)}`);
   return new SubscriptionDecryptionVO(
     SubscriptionDecryptionCommand.fromDto(request.body.xml, request.query, token, encodingAESKey)
   );
